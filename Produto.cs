be18c335975c702ed54e3358b0f4f94a763bce93
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-01-18 11:04:30 -0300

    Adicionar arquivos de projeto.

---
+++ Produto.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Treino1
+{
+    class Produto
+    {
+        #region Atributos
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+        #endregion
+
+        #region Construtores
+        public Produto()
+        {
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+        #endregion
+
+        #region GetSetNome
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if(value != null && value.Length < 1)
+                {
+                    Console.WriteLine("Nome do produto não pode ser vazio e nem conter menos que ou apenas um caractere!");
+                }
+                else
+                {
+                    _nome = value;
+                }
+            }
+        }
+        #endregion
+
+        #region GetSetPreco
+        public double Preco
+        {
+            get { return _preco; }
+            set { _preco = value; }
+        }
+        #endregion
+
+        #region GetSetQuantidade
+        public int Quantidade
+        {
+            get { return _quantidade; }
+            set { _quantidade = value; }
+        }
+        #endregion
+
+        #region Metodos
+        public void AdcionarProduto(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+        public void RemoveProduto(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+
+        public double ValorTotal()
+        {
+            return _quantidade * _preco;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome do Produto: {Nome}, " +
+                $"Valor Unitario: {Preco}, " +
+                $"Quantidade no estoque: {Quantidade}";
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Treino1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p1 = new Produto();
+            Inicio:
+            Console.WriteLine("Entre com os Dados do produto:");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Preço Unitario: R$");
+            p1.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            p1.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine(p1);
+
+            Console.WriteLine("Deseja Proseguir para pagamento? (s/n)");
+            char resposta = char.Parse(Console.ReadLine());
+            if(resposta == 'n' || resposta == 'N')
+            {
+                Console.WriteLine("Ação Cancelada pelo Usuario!");
+                goto Inicio;
+            }
+            else if(resposta == 's' || resposta == 'S')
+            {
+                Console.Clear();
+                Console.WriteLine(p1);
+                Console.WriteLine(p1.ValorTotal().ToString("F2", CultureInfo.InvariantCulture));
+                Pagamento:
+                Console.WriteLine("Confirma o valor total da compra? (s/n)");
+                char resposta2 = char.Parse(Console.ReadLine());
+                if(resposta2 == 'n' || resposta2 == 'N')
+                {
+                    Console.WriteLine("Ação Cancelada pelo Usuario!");
+                    goto Inicio;
+                }
+                else if(resposta2 == 's' || resposta2 == 'S')
+                {
+                    Console.Clear();
+                    Console.WriteLine("Compra Efetuada com sucesso!");
+                    Console.WriteLine(p1);
+                    Console.WriteLine(p1.ValorTotal());
+                }
+                else
+                {
+                    Console.WriteLine("Digite uma opção valida!");
+                    goto Pagamento;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
